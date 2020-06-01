9d19503f891def71b7db43148e74b8e09d3aeac3
Administrator <Administrator@PC200>
2020-06-01 16:33:49 +0700

    thu 2

---
+++ BigSchool/Controllers/CoursesController.cs
@@ -0,0 +1,29 @@
+﻿using BigSchool.Models;
+using BigSchool.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using System.Web.Mvc;
+using System.Linq;
+
+namespace BigSchool.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        // GET: Courses
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
--- BigSchool/Models/ApplicationUser.cs
+++ BigSchool/Models/ApplicationUser.cs
@@ -17,17 +17,5 @@ namespace BigSchool.Models
             return userIdentity;
         }
     }
-
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
-        public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
-        {
-        }
-
-        public static ApplicationDbContext Create()
-        {
-            return new ApplicationDbContext();
-        }
-    }
+    
 }
